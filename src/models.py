8e2eabc57495192b682abe7d094e67f3df10fbb1
Alejandro Vielma Rondon <alejandrovir22@gmail.com>
2026-04-23 13:25:04 +0000

    Alejandro

--- src/models.py
+++ src/models.py
@@ -1,19 +1,39 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, ForeignKey, Integer
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
+    __tablename__ = 'user'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(
+        String(80), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(80), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class Post(db.Model):
+    __tablename__ = 'post'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    image_url: Mapped[str] = mapped_column(String(250), nullable=False)
+    caption: Mapped[str] = mapped_column(String(250))
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('user.id'))
+
+
+class Comment(db.Model):
+    __tablename__ = 'comment'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    comment_text: Mapped[str] = mapped_column(String(250), nullable=False)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('user.id'))
+    post_id: Mapped[int] = mapped_column(Integer, ForeignKey('post.id'))
+
+
+class Media(db.Model):
+    __tablename__ = 'media'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    url: Mapped[str] = mapped_column(String(250), nullable=False)
+    post_id: Mapped[int] = mapped_column(Integer, ForeignKey('post.id'))
